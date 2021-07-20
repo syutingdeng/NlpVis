97796c80a6247c181845532f896c39d4990a49cf
Syu,Ting Deng <billy12349932@gmail.com>
2021-07-20 11:53:03 +0800

    Umap with CLS 20210720

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import dataclasses
 from transformers import BertTokenizer, BertForSequenceClassification,BertConfig
 import torch
 from transformers import pipeline
@@ -14,9 +15,9 @@ class TransformersModel:
     def __init__(self,model_name,UserInput):
         self.model_name=model_name
         self.tokenizer = BertTokenizer.from_pretrained(model_name)
-        self.UserInput = UserInput
+        self.UserInput = UserInput ####user input
+        self.data = loaddata() ###load from dataset
     
-
     def setModel(self,layer):
         config = BertConfig(num_hidden_layers=layer,output_hidden_states=True)
         self.model = BertForSequenceClassification(config)
@@ -24,15 +25,34 @@ class TransformersModel:
         print("Model name"+self.model_name+"\n")
         print(self.model)
 
-    def getHiddenState(self,level):
+    def getHiddenState(self,level):# get uesr input hiddenstate
         inputs = self.tokenizer(self.UserInput, return_tensors="pt")
         labels = torch.tensor([1]).unsqueeze(0)  # Batch size 1
         outputs = self.model(**inputs, labels=labels)
         self.hidden_states = outputs.hidden_states[level][0]
         self.length = outputs.hidden_states[level][0].shape[0]
-        self.level = level
         print("Hidden Stat layer"+str(level))
+        
         return outputs.hidden_states[level][0]
+    
+    def getDataSetHiddenState(self,level): # get dataset "cls" hiddenstate
+        cls_embedding = []
+        for index,item in enumerate(self.data):
+            if(index%10==0):
+                print("#",str(index))
+            inputs = self.tokenizer(item, return_tensors="pt",padding='max_length')
+            labels = torch.tensor([1]).unsqueeze(0)  # Batch size 1
+            outputs = self.model(**inputs, labels=labels)
+
+            #print("[CLS]embedding:")     
+            #print(outputs.hidden_states[level][0][0])
+            cls_embedding.append(outputs.hidden_states[level][0][0])##CLS embedding
+        self.cls_embedding = cls_embedding
+        self.dataset_length = len(cls_embedding)
+        print("cls_embedding length",str(self.dataset_length))
+       
+
+
 
     def classify(self):
         classifier = pipeline('sentiment-analysis', model=self.model, tokenizer=self.tokenizer)
@@ -42,22 +62,32 @@ class TransformersModel:
     def umap(self):
         reducer = umap.UMAP(random_state=42)
         word_embedding = []
-        for i in range(self.length):    
-            word_embedding.append(self.hidden_states[i].detach().numpy())
+        for i in range(self.dataset_length):
+            word_embedding.append(self.cls_embedding[i].detach().numpy())
+        #print(word_embedding)
         word_embedding = np.array(word_embedding)
         word_embedded_reduce = reducer.fit_transform(word_embedding)
+
+        """for i in range(self.length):    
+            word_embedding.append(self.hidden_states[i].detach().numpy())
+        print(word_embedding)
+        word_embedding = np.array(word_embedding)
+        word_embedded_reduce = reducer.fit_transform(word_embedding)"""
         
+    
         x=[]
         y=[]
         x_min, x_max =  word_embedded_reduce.min(0), word_embedded_reduce.max(0)
         a_norm = (word_embedded_reduce - x_min) / (x_max - x_min)
-        for i in a_norm:
+        
+        for i in word_embedded_reduce:
             x.append(i[0])
             y.append(i[1])
         print(x)
         print(y)
         plt.plot(x,y,'ro')
         plt.show()
+
     
     
 
@@ -66,7 +96,8 @@ class TransformersModel:
 
 model0 = TransformersModel('bert-base-uncased',"UMAP is a general purpose manifold learning and dimension reduction algorithm. It is designed to be compatible with scikit-learn, making use of the same API and able to be added to sklearn pipelines.")
 model0.setModel(6)
-model0.getHiddenState(3)
-#model0.umap()
+#model0.getHiddenState(3)
+model0.getDataSetHiddenState(6)
+model0.umap()
 
 print(model0.classify())
--- prepare_data.py
+++ prepare_data.py
@@ -4,11 +4,14 @@ def loaddata():
     dataset = load_dataset('glue', 'mrpc', split='train')
     data= []
     #esult= dataset.filter(lambda example: example['label'] == dataset.features['label'].str2int('equivalent'))[1]
-    for i in range(len(dataset)):
-        data.append(dataset[i]["sentence1"])
+    for i in range(50):
+        result = "[CLS] "+dataset[i]["sentence1"]
+        data.append(result)
+    print(data[0][0])   
     return data
 
-print(loaddata())
+if __name__ == '__main__':
+   print(loaddata())
    
 
 
